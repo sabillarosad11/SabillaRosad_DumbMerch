efdecaa884a87f3464fe45b0df0150a3738ca530
sabillarosad11 <sabillarosad114@gmail.com>
2022-03-31 10:57:45 +0700

    add register

---
+++ Batch32-FrontEnd/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import Login from './Pages/Login'
+import Register from "./Pages/Register";
+import './App.css';
+
+function App() {
+  return (
+    <Register />
+  );
+}
+
+export default App;
